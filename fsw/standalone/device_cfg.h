313cda302cf00a6292a2601b7e4d71e832c7f57f
dacarter22 <davidacarter22@gmail.com>
2025-02-04 13:18:48 -0500

    [nasa/nos3#467-generic_star_tracker_checkout] Edited device_cfg to match platform cfg file

--- fsw/standalone/device_cfg.h
+++ fsw/standalone/device_cfg.h
@@ -6,10 +6,10 @@
 */
 #define GENERIC_STAR_TRACKER_CFG
 /* Note: NOS3 uart requires matching handle and bus number */
-#define GENERIC_STAR_TRACKER_CFG_STRING           "/dev/usart_16"
-#define GENERIC_STAR_TRACKER_CFG_HANDLE           16 
+#define GENERIC_STAR_TRACKER_CFG_STRING           "usart_10"
+#define GENERIC_STAR_TRACKER_CFG_HANDLE           10
 #define GENERIC_STAR_TRACKER_CFG_BAUDRATE_HZ      115200
-#define GENERIC_STAR_TRACKER_CFG_MS_TIMEOUT       250
+#define GENERIC_STAR_TRACKER_CFG_MS_TIMEOUT       50
 #define GENERIC_STAR_TRACKER_CFG_DEBUG
 
 #endif /* _GENERIC_STAR_TRACKER_CHECKOUT_DEVICE_CFG_H_ */
